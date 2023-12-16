0f05ef36ddc65a002c07e664cc05709983ff2a23
Madhu Lokesh <madhumj623@gmail.com>
2023-12-16 23:58:24 +0530

    Save data in Firestore database

--- app/(dashboard)/(routes)/upload/page.js
+++ app/(dashboard)/(routes)/upload/page.js
@@ -1,13 +1,20 @@
 "use client"
-import React , {useState} from 'react'
+import React , {useEffect, useState} from 'react'
 import UploadForm from './_components/UploadForm'
 import { app } from '@/firebaseConfig'
 import { getStorage, ref, uploadBytesResumable, getDownloadURL} from "firebase/storage";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
+import { useUser } from '@clerk/nextjs';
+import { generateRandomString } from '@/app/_utils/GenerateRandomString';
 
+const storage = getStorage(app);
+const db = getFirestore(app);
 
 export default function Upload() {
+
+  const {user}= useUser();
   const [progress, setProgress] = useState();
-  const storage = getStorage(app)
+  const [uploadCompleted, setUploadCompleted] = useState(false);
   const uploadFile=(file) => {
     const metadata = {
       contentType: file.type
@@ -23,14 +30,47 @@ export default function Upload() {
     setProgress(progress)
     progress == 100 && getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
       console.log('File available at', downloadURL);
+      saveInfo(file, downloadURL)
     });
   },
   (error) =>{
     console.log("Error during upload", error)
-  },
-  () => {
-    console.log('Uploaded completed successfully')
   })
+
+  const saveInfo= async (file, fileUrl) =>{
+    const docId = generateRandomString().toString();
+
+    await setDoc(doc(db, "uploadedFile", docId), {
+        filename: file?.name,
+        fileSize: file?.size,
+        fileType: file?.type,
+        fileUrl: fileUrl,
+        userEmail:user?.primaryEmailAddress.emailAddress,
+        username: user?.fullName,
+        password: '',
+        id: docId,
+        shortUrl:process.env.NEXT_PUBLIC_BASE_URL+docId
+    }).then(res =>{
+      console.log(res)
+    })
+  }
+
+  useEffect(() =>{
+    console.log("Trigger")
+
+    progress==100 && setTimeout(() =>{
+      setUploadCompleted(true)
+    }, 2000)
+  }, [progress])
+
+  useEffect(() => {
+    uploadCompleted && 
+    setTimeout(() => {
+      setUploadCompleted(false);
+      window.location.reload();
+    }, 2000)
+  }, [uploadCompleted])
+
 }
   return (
     <div className=' p-5 px-8 md:px-28 text-center'>
---
+++ app/_utils/GenerateRandomString.js
@@ -0,0 +1,9 @@
+export const generateRandomString=() =>{
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz0123456789"; 
+  let result ='';
+  for (let i=0; i<6; i++)
+  {
+    result +=characters.charAt(Math.floor(Math.random()*characters.length));
+  } 
+  return result
+}
